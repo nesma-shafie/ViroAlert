bf71e3b8a34f89744287a511212d27a8122db7fa
nesma-shafie <nesmashafie342@gmail.com>
2025-06-25 12:58:33 +0300

    GAN API

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { predictHost,predictAntivirus } from "../controllers/userController.js";
+import { predictHost,predictAntivirus ,generateAntiVirus} from "../controllers/userController.js";
 import { auth } from "../middlewares/auth.js";
 import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
@@ -8,6 +8,7 @@ const userRouter = Router();
 
 userRouter.route('/predictHost').post(auth,upload.single('file'), predictHost);
 userRouter.route('/predictAntiVirus').post(auth, upload.single('file'), predictAntivirus);
+userRouter.route('/generateAntiVirus').post(auth, upload.single('file'), generateAntiVirus);
 
 
 

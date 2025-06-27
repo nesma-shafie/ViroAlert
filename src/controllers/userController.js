6518c7d11dde3f91e323a4f35de6bf026f330920
Yousef Osama <93356614+yousefosama654@users.noreply.github.com>
2025-06-27 17:20:47 +0300

    Classification and Drug works pretty fine Back

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,11 +4,23 @@ import fs from 'fs';
 import { getKmers, jaccardSimilarity, align2seq } from '../utils/align.js';
 import prisma from '../prisma.js'; // Assuming you have a Prisma client setup
 const predictHost = async (req, res) => {
-  const filePath = req.file.path;  // assuming multer stores it here
-  const fileName = req.file.originalname;
-
+  // const filePath = req.file.path;  // assuming multer stores it here
+  // const fileName = req.file.originalname;
   const form = new FormData();
-  form.append('file', fs.createReadStream(filePath), fileName);
+  if (req.file) {
+    const filePath = req.file.path;
+    const fileName = req.file.originalname;
+
+    form.append('file', fs.createReadStream(filePath), fileName);
+  }
+
+  // If virus sequence is provided manually (and not file)
+  if (!req.file && req.body.virus) {
+    form.append('virus', req.body.virus);
+  }
+  // form.append('file', fs.createReadStream(filePath), fileName);
+
+  // form.append('file', fs.createReadStream(filePath), fileName);
 
   const url = process.env.FASTAPI_URL + '/predict-host'; // Ensure you have this environment variable set
   const fastapiResponse = await axios.post(url, form, {
@@ -17,15 +29,29 @@ const predictHost = async (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    data: fastapiResponse.data,
+    probability: (1 - fastapiResponse.data.probability[0]) * 100,
+    img: fastapiResponse.data.img,
   });
 }
 const predictHost_ML = async (req, res) => {
-  const filePath = req.file.path;  // assuming multer stores it here
-  const fileName = req.file.originalname;
+  // const filePath = req.file.path;  // assuming multer stores it here
+  // const fileName = req.file.originalname;
 
   const form = new FormData();
-  form.append('file', fs.createReadStream(filePath), fileName);
+
+  // If FASTA file is uploaded
+  if (req.file) {
+    const filePath = req.file.path;
+    const fileName = req.file.originalname;
+
+    form.append('file', fs.createReadStream(filePath), fileName);
+  }
+
+  // If virus sequence is provided manually (and not file)
+  if (!req.file && req.body.virus) {
+    form.append('virus', req.body.virus);
+  }
+  // form.append('file', fs.createReadStream(filePath), fileName);
 
   const url = process.env.FASTAPI_URL + '/predict-host-ML'; // Ensure you have this environment variable set
   const fastapiResponse = await axios.post(url, form, {
@@ -34,7 +60,8 @@ const predictHost_ML = async (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    data: fastapiResponse.data,
+    probability: fastapiResponse.data.probability * 100,
+
   });
 }
 
@@ -65,7 +92,7 @@ const predictAntivirus = async (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    data: fastapiResponse.data,
+    data: fastapiResponse.data
   });
 }
 
@@ -92,7 +119,7 @@ const topAntivirus = async (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    data: fastapiResponse.data,
+    smiles: fastapiResponse.data.top_smiles,
   });
 }
 
@@ -119,7 +146,7 @@ const generateAntiVirus = async (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    data: fastapiResponse.data,
+    smiles: fastapiResponse.data.drugs,
   });
 }
 const align = async (req, res) => {
@@ -167,13 +194,12 @@ const align = async (req, res) => {
       const jaccard = jaccardSimilarity(inputKmers, targetKmers);
       return { ...entry, jaccard };
     });
-    let sim=0.2;
-     let    finalfiltered= filtered.filter(entry => entry.jaccard >= 0.2);
+  let sim = 0.2;
+  let finalfiltered = filtered.filter(entry => entry.jaccard >= 0.2);
 
-    while(filtered.length<5)
-   { 
+  while (filtered.length < 5) {
     sim -= 0.05;
-    finalfiltered= filtered.filter(entry => entry.jaccard >= sim);
+    finalfiltered = filtered.filter(entry => entry.jaccard >= sim);
   }
   console.log(filtered.length);
   // Sort by Jaccard descending
@@ -181,7 +207,7 @@ const align = async (req, res) => {
 
   // Step 2: Run alignment only on the top N from filtered
   const results = filtered.slice(0, 30).map(entry => {
-    const {alignedSeq1,alignedSeq2,matchLine,score} = align2seq(input_sequence, entry.sequence); // fast version
+    const { alignedSeq1, alignedSeq2, matchLine, score } = align2seq(input_sequence, entry.sequence); // fast version
     return { label: entry.label, sequence: entry.sequence, jaccard: entry.jaccard, score, alignedSeq1, alignedSeq2, matchLine };
   });
 

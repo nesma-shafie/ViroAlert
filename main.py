40370586aab4ac1d4ba56a74cb6a4f0aeae4d70f
Eman <ibrahim.eman83@gmail.com>
2025-06-27 22:21:35 +0300

    Add predict host ML route

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from model import GatedAttention,DrugTargetGNN
 import esm
 from typing import Optional
 from utils import read_virus,read_virus_seqs,test_one_virus,test_antivirus,test_top_antivirus,visualize_attention_2d_heatmaps
+from ML_model import Virus2Vec, get_predection_per_virus
 import argparse
 torch.serialization.add_safe_globals([argparse.Namespace])
 from GAN_utils import get_new_antivirus
 from GAN_model import Generator
+import joblib
 
 
 # CONSTANTS
@@ -50,6 +52,7 @@ antivirus_model = DrugTargetGNN()
 antivirus_model.load_state_dict(torch.load(r"models\drug_target_model2.pth", map_location=device))
 esm_model, esm_alphabet=esm.pretrained.load_model_and_alphabet_local(r"models\esm1b_t33_650M_UR50S.pt")
 esm_model.eval()
+Virus2VecModel=joblib.load(r"models\RF_Virus2vec.pkl")
 
 checkpoint_path=r"models\checkpoint_epoch_38.pt"
 checkpoint = torch.load(checkpoint_path, map_location=device)
@@ -67,13 +70,35 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+@app.post("/predict-host-ML")
+async def predict_host_ML(file: Optional[UploadFile] = File(None),
+    virus: Optional[str] = Form(None)):
+    if file:
+        content = (await file.read()).decode()
+        virus = read_virus_seqs(content)
+    # Case 2: Use virus sequence from form field
+    elif virus:
+        virus = read_virus_seqs(virus)
 
+    datas = virus["Sequence"]
+    Virus2Vec_feature_vector = Virus2Vec(datas)
+
+    #  Make prediction
+    Y_prob = get_predection_per_virus(Virus2VecModel,Virus2Vec_feature_vector)
+    print(f"Y_prob: {Y_prob}")
+    return {
+        "probability": Y_prob,
+    }
 @app.post("/predict-host")
-async def predict_host(file: UploadFile=File(...)):
-    
-    content = (await file.read()).decode()
-    # Read the virus data
-    virus = read_virus_seqs(content)
+async def predict_host(file: Optional[UploadFile] = File(None),
+    virus: Optional[str] = Form(None)):
+        # Case 1: File is uploaded (FASTA)
+    if file:
+        content = (await file.read()).decode()
+        virus = read_virus_seqs(content)
+    # Case 2: Use virus sequence from form field
+    elif virus:
+        virus = read_virus_seqs(virus)
 
     ids_original = virus["Virus_ID"]
     datas = virus["Sequence"]
@@ -84,17 +109,16 @@ async def predict_host(file: UploadFile=File(...)):
 
     #  Make prediction
     Y_prob, Y_hat, A, A_2 = test_one_virus(model,ft_model,datas,ids,seq_ids)
+
     A = [a.cpu().numpy() if isinstance(a, torch.Tensor) and a.is_cuda else a.numpy() if isinstance(a, torch.Tensor) else np.array(a) for a in A]
     A_2 = [a_2.cpu().numpy() if isinstance(a_2, torch.Tensor) and a_2.is_cuda else a_2.numpy() if isinstance(a_2, torch.Tensor) else np.array(a_2) for a_2 in A_2]
     A = np.array(A)
     A_2 = np.array(A_2)      
     base64_img = visualize_attention_2d_heatmaps(A, A_2, seq_ids_original, ids_original)
-
     return {
-        "prediction": Y_hat.tolist()[0],
         "probability": Y_prob.tolist()[0],
         "img":base64_img
-}
+    }
 
 @app.post("/predict-antivirus")
 async def predict_antivirus(
